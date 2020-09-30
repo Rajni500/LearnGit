d10c0d1d855e7bb6ddb86bab0b65a25740a3e640
Emeis DevOps <devops@emeistechnologies.com>
2020-09-30 18:03:09 +0530

    Add project files.

---
+++ BasicProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BasicProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Test.MyProperty);
+            Console.ReadKey();
+        }
+    }
+
+    public class Test
+    {
+        public static int MyProperty { get; set; }
+
+        public Test()
+        {
+           
+        }
+    }
+}
